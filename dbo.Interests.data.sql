1235a1f9c5736aab146de3531519fe489043c650
Deltaforcecis497 <43114915+Deltaforcecis497@users.noreply.github.com>
2018-11-28 14:38:35 -0600

    Add files via upload

---
+++ dbo.Interests.data.sql
@@ -0,0 +1,6 @@
+SET IDENTITY_INSERT [dbo].[Interests] ON
+INSERT INTO [dbo].[Interests] ([InterestId], [topic]) VALUES (1, N'providence hospital')
+INSERT INTO [dbo].[Interests] ([InterestId], [topic]) VALUES (2, N'springhill medical center')
+INSERT INTO [dbo].[Interests] ([InterestId], [topic]) VALUES (3, N'usa health university hospital')
+INSERT INTO [dbo].[Interests] ([InterestId], [topic]) VALUES (4, N'baypointe hospital')
+SET IDENTITY_INSERT [dbo].[Interests] OFF
